82d39661373320cca33bf003bdea8dab1eef0e46
Chad Hoosain <chad@bnry.digital>
2025-08-20 18:54:19 +0200

    `Update navdata.js with new URL paths`

--- src/data/navdata.js
+++ src/data/navdata.js
@@ -1,4 +1,4 @@
 export const navdata = [
-  { label: "about", url: "/about", type: "link" },
-  { label: "Gallery", url: "/gallery", type: "link" },
+  { label: "about", url: "/happypawss-website//about", type: "link" },
+  { label: "Gallery", url: "/happypawss-website/gallery", type: "link" },
 ];
