aa42cbf66bcd5b6de9a1e377225532dace9a0648
Chad Hoosain <chad@bnry.digital>
2025-08-20 16:12:09 +0200

    `Update next.config.mjs: add basePath and assetPrefix`

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // tells Next.js to do a static export
-  // basePath: "/happypawss-website",
+  basePath: "/happypawss-website",
+  assetPrefix: "/happypawss-website/",
 };
 
 export default nextConfig;
